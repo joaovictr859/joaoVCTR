7d47fc1b7248a3070bcbf95e9af35926c00039f4
joaovictr859 <joao.maia.713@ufrn.edu.br>
2026-06-15 23:14:40 -0300

    Add files via upload

--- trabalho0.3.py
+++ trabalho0.3.py
@@ -1,7 +1,76 @@
 from os import system
 from time import sleep
+import pickle
 
-# BANCO DE DADOS EM MEMÓRIA (Dicionário para salvar os produtos cadastrados)
+def existe(arquivo):
+    try:
+        file = open(arquivo,'rb')
+        file.close()
+        return True
+    except FileNotFoundError:
+        return False
+    except:
+        return None
+
+def criar(arquivo):
+    try:
+        file = open(arquivo,'wb')
+        file.close()
+        return True
+    except:
+        return False
+
+def ler(arquivo):
+    try:
+        file = open(arquivo,'rb')
+        dicionario = pickle.load(arquivo)
+        file.close()
+        return dicionario
+    except:
+        return None
+
+def atualizar(arquivo,dicio):
+    try:
+        file = open(arquivo,'wb')
+        pickle.dump(dicio,file)
+        file.close()
+        return True
+    except:
+        return False
+
+####################################################################
+
+#atualizar
+#ler
+#criar
+#existe
+salvar_interno = False
+arquivo = 'cardapio_dados.dat'
+prosseguir1 = existe(arquivo)
+if prosseguir1: #existe
+    print('Arquivo existente!')
+
+elif (prosseguir1 == False): #criar
+    prosseguir2 = criar(arquivo)
+    if not(prosseguir2): #erro na criação
+        print('Erro na na criação do arquivo')
+        salvar_interno = True
+    else:
+        print('Arquivo criado com sucesso!')
+else: #erro na identificaçao
+    print('Erro na identificação do arquivo')
+    salvar_interno = True
+    
+if salvar_interno:
+    print('Manipulação de arquivo externo com erro...modo salvamento interno')
+
+else:
+    print('Manipulação de arquivo externo feita com sucesso!')
+
+
+##################################################################################
+
+
 cardapio_dados = {
     "hamburguer de siri": {
         "nome_exibicao": "Hamburguer de Siri",
@@ -17,12 +86,6 @@ cardapio_dados = {
     }
 }
 
-l = []
-a = []
-for x in range(9):
-    l.append(0)
-for x in range(5):
-    a.append(0)
 
 titulo = """\033[1;33m
 ████  █   █ ████   ███   ████ ████  █   █
@@ -84,29 +147,29 @@ while resp != '7':
     
     if resp == '2':
         nome = input("Insira o nome do produto para atualizar: ").strip()
-        nome_chave = nome.lower() # Padroniza para buscar sem dar erro de maiúsculas
+        nome_chave = nome.lower() 
         
-        # O "U" do CRUD: Verifica se o produto existe no banco de dados
+       
         if nome_chave in cardapio_dados:
             print(f"\033[1;32mPRODUTO ENCONTRADO ({cardapio_dados[nome_chave]['nome_exibicao']})!\033[m")
             
-            # Pede os novos dados
-            preco = input(f"Insira o novo preço: R$ ")
-            tempo = input(f"Insira o novo tempo médio de preparo: ")
             
-            # Coleta os novos ingredientes de forma correta
+            preco = input("Insira o novo preço: R$ ")
+            tempo = input("Insira o novo tempo médio de preparo: ")
+            
+           
             ingredientes = []
             print("==== DIGITE 0 PARA PARAR ====")
             while True:
                 ing = input(f"Insira o {len(ingredientes) + 1}° ingrediente: ").strip()
                 if ing == '0':
-                    break # Para o loop sem colocar o '0' dentro da lista
+                    break 
                 if ing != "":
                     ingredientes.append(ing)
             
-            # ATUALIZAÇÃO NO DICIONÁRIO: Substitui os dados antigos pelos novos
+            
             cardapio_dados[nome_chave] = {
-                "nome_exibicao": cardapio_dados[nome_chave]['nome_exibicao'], # Mantém o nome original bonito
+                "nome_exibicao": cardapio_dados[nome_chave]['nome_exibicao'], 
                 "preco": preco,
                 "tempo": tempo,
                 "ingredientes": ingredientes
@@ -114,20 +177,20 @@ while resp != '7':
             
             print("\033[1;32mATUALIZADO COM SUCESSO! AGUARDANDO O RETORNO\033[m")
         else:
-            # Se o produto não existir no dicionário:
+            
             print("\033[1;31mPRODUTO NÃO ENCONTRADO NO SISTEMA!\033[m")
             
         sleep(3)
     
     elif resp == '3':
         nome = input("Insira o nome do produto para pesquisar: ").strip()
-        nome_chave = nome.lower() # Padroniza para buscar o item mesmo se digitar Maiúsculo/Minúsculo
+        nome_chave = nome.lower() 
         
-        # O "R" (Read) do CRUD: Verifica se o lanche existe nas chaves do dicionário
+        
         if nome_chave in cardapio_dados:
             produto = cardapio_dados[nome_chave]
             
-            # Pega a lista de ingredientes e transforma em um texto bonito separado por vírgulas
+            
             lista_ingredientes = ", ".join(produto['ingredientes'])
             
             print("\033[1;32mPRODUTO ACHADO COM SUCESSO!\033[m")
@@ -138,7 +201,7 @@ while resp != '7':
             print(f"INGREDIENTES    | {lista_ingredientes}")
             print("============================================")
         else:
-            # Caso o usuário digite algo que não está no dicionário
+            
             print("\033[1;31mPRODUTO NÃO ENCONTRADO NO CARDÁPIO!\033[m")
         
         print("\nESPERANDO PARA LEITURA DO USUÁRIO")
@@ -146,23 +209,23 @@ while resp != '7':
         
     if resp == '4':
         nome = input("Insira o nome do produto para deletar: ").strip()
-        nome_chave = nome.lower() # Padroniza a string para evitar erros de maiúsculas/minúsculas
+        nome_chave = nome.lower() 
         
-        # O "D" (Delete) do CRUD: Verifica se o lanche existe nas chaves do dicionário
+       
         if nome_chave in cardapio_dados:
             
-            # Executa a remoção real do item do "banco de dados"
+            
             del cardapio_dados[nome_chave]
             
             print(f"\033[1;32mPRODUTO '{nome}' ENCONTRADO E DELETADO COM SUCESSO!\033[m")
         else:
-            # Se o usuário digitar um nome que não existe no dicionário:
+           
             print("\033[1;31mPRODUTO NÃO ENCONTRADO NO SISTEMA!\033[m")
             
         print("AGUARDANDO O RETORNO!")
         sleep(3)
 
-    elif resp == '5': #cardapio
+    elif resp == '5':
         print("""
         ===========================
         BAURU       R$6
